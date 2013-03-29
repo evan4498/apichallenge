8580ccd321c061993c10c9726beaae3b6d5f2f00
Evan <evan.ochs@rackspace.com>
2013-03-29 09:46:33 -0500

    challenge4 updated with functions

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# Copyright 2012 Rackspace
+
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import os
 import sys
 import time
@@ -8,6 +24,15 @@ creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 pyrax.set_credential_file(creds_file)
 cs = pyrax.cloudservers
 
+def main():
+  csnumber = int(raw_input ("How many servers should I create: "))
+  csbasename = str(raw_input ("What do you want the servers to be named: "))
+  print "This should take a few minutes, and possibly as much as 12 parsecs..."
+  total = 0
+  while total < csnumber:
+    total = total + 1
+    csname = csbasename + str(total)
+    createserver(csname)
 
 def createserver(csname):
   csimage = "d531a2dd-7ae9-4407-bb5a-e5ea03303d98"
@@ -30,20 +55,11 @@ def showdetails(server,rootpass):
     for pubaddr in  serverinfo.addresses["public"]:
       print "Public Network:", pubaddr['addr']
     print "-------------------------------------------------"
-
   else:
     time.sleep(10)
     showdetails(serverinfo,rootpass)
 
-def main():
-  csnumber = int(raw_input ("How many servers should I create: "))
-  csbasename = str(raw_input ("What do you want the servers to be named: "))
-  print "This should take a few minutes, and possibly as much as 12 parsecs..."
-  total = 0
-  while total < csnumber:
-    total = total + 1
-    csname = csbasename + str(total)
-    createserver(csname)
+
 
 if __name__ == "__main__":
   main()
--- challenge4.py
+++ challenge4.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# Copyright 2012 Rackspace
+
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import os
 import sys
 import time
@@ -15,7 +31,7 @@ def main():
   print "--------------------------------------------------"
   domslist = dns.list()
   for pos, domain in enumerate(domslist):
-    print "%s: %s" % (pos, domain)
+    print "%s: %s" % (pos, domain.name)
   
   dom_choice = int(raw_input("Select the number for the domain to use: "))
   domain = domslist[dom_choice]
